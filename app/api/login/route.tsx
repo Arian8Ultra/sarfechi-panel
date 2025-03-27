1db96cbebf4ae17861a5d89af4f89cd3134bf7d9
Arian Rezaei <arian8ultra@gmail.com>
2025-03-27 13:54:00 +0330

    Implement POST API for user login with phone number validation and token management

--- app/api/login/route.tsx
+++ app/api/login/route.tsx
@@ -1,11 +1,70 @@
-// export async function GET(request: Request) {}
+import { Admin_Login } from "@/api/Admin";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-// export async function HEAD(request: Request) {}
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { phone_number, password } = body;
+  const cookie = await cookies();
 
-// export async function POST(request: Request) {}
+  const phoneNumber = (phone_number as string).startsWith("0")
+    ? phone_number?.replace("0", "")
+    : phone_number;
+  if (phoneNumber.length < 10) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "شماره موبایل باید 11 رقم باشد",
+        data: null,
+      },
+      {
+        status: 400,
+      },
+    );
+  }
 
-// export async function PUT(request: Request) {}
-
-// export async function DELETE(request: Request) {}
-
-// export async function PATCH(request: Request) {}
+  try {
+    const res = await Admin_Login(phoneNumber, password);
+    console.log(res);
+    if (res.success) {
+      cookie.set("access_token", res.data.access_token, {
+        expires: new Date(Date.now() + res.data.expires_in * 1000),
+      });
+      cookie.set("refresh_token", res.data.refresh_token, {
+        expires: new Date(Date.now() + res.data.expires_in * 1000),
+      });
+      return NextResponse.json(
+        {
+          success: true,
+          message: res.message,
+          data: res.data,
+        },
+        {
+          status: 200,
+        },
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: res.message,
+          data: null,
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: error,
+        data: null,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
